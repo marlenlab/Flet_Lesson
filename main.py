05fc1d026b7591e4e1ec8e283ce155e30dc963f2
marlenlab <marlenmarlisovich@gmail.com>
2026-05-13 11:43:46 +0000

    added Lesson2

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-import flet as ft
+import flet as ft # Импортируем и переимениуем (сокращаем на ft)
 
+# Создание Страницы С помощью 
 def main_page(page: ft.Page):
-    #Hello_Text = 'Hello Gekss'
-    page.title = 'My first Project'
+
+    # Создаем и задаем тему для страницы
     page.theme_mode = ft.ThemeMode.LIGHT
-
-    Hello_Text = ft.Text(value='Hello, World')
-
-    name_input = ft.TextField(label='Введите имя')
-
-
-    page.add(Hello_Text, name_input)
     
+    #
+    page.title = 'Мое первое приложение'
+    #
+    greteng = ft.Text(value="Hello")
+    #
+    name_input = ft.TextField(label="Введите имя:")
+    #
+    page.add(greteng, name_input)
+
+#
 ft.app(main_page)
-#ft.app(main_page, view=ft.AppView.WEB_BROWSER)
+
+# ft.app(main_page , view = ft.AppView.WEB_BROWSER)
